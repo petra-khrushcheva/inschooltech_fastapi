987a3b9800c6ae8ab5ae629b9de78e21fcfd1f01
Petra Khrushcheva <petra.khrushcheva@gmail.com>
2024-03-03 18:31:50 +0500

    fix auth dependency

--- src/router/api_v1.py
+++ src/router/api_v1.py
@@ -1,9 +1,8 @@
 from fastapi import APIRouter, Depends
 
 from src.public.router import router as public_router
-from src.users.router import router as users_router
-
 from src.users.dependencies import current_active_user as current_user
+from src.users.router import router as users_router
 
 router = APIRouter(prefix="/v1", dependencies=[Depends(current_user)])
 router.include_router(public_router)
