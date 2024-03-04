51b66d58d10d5c8374bd33fc737ecdf582318bf1
Petra Khrushcheva <petra.khrushcheva@gmail.com>
2024-03-04 19:47:14 +0500

    fix numbers to strings score pydantic schema

--- src/indicators/schemas.py
+++ src/indicators/schemas.py
@@ -4,7 +4,7 @@ from pydantic import BaseModel, ConfigDict
 
 
 class Score(BaseModel):
-    model_config = ConfigDict(from_attributes=True)
+    model_config = ConfigDict(from_attributes=True, coerce_numbers_to_str=True)
 
     id: UUID
     score: str
